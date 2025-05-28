b52347c6ef7b24ce19a1ba6b3bc2e4b2cc619546
ErebosXYZ <robertmaynou@gmail.com>
2025-05-28 20:55:00 +0200

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 
 // Middleware
@@ -16,14 +16,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Cargar preguntas desde JSON. En esta variable dispones siempre de todasl as preguntas de la "base de datos"
 const questions = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'));
 
+// Creo una funció que generi un índex aleatori d'un array i el retorni
+function randomQuestion(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+// console.log(questions)
+console.log(randomQuestion(questions));
+
+let categories = questions.map((q) => q.category);
+
+categories = [...new Set(categories)];
+console.log(categories);
+
+
+
 // Endpoint para obtener una pregunta aleatoria (con filtro por categoría)
 app.get('/api/question', (req, res) => {
-  res.send('Falta implementar el endpoint /api/question');
+  let category = req.query.category;
+  if (!category){
+      return res.json(randomQuestion(questions));
+  };
+  const filteredQuestions = questions.filter(q => q.category && q.category.toLowerCase() === category.toLowerCase());
+  if (filteredQuestions.length === 0) {
+    return res.status(404).json({error: 'Error 404: no hay preguntas para la categoría solicitada'})
+  } 
+
+  res.json((randomQuestion(filteredQuestions)))
+
 });
 
 // Endpoint para obtener categorías únicas
 app.get('/api/categories', (req, res) => {
-  res.send('Falta implementar el endpoint /api/categories');
+  res.json(categories);
 });
 
 
